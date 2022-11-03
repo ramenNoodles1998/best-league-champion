d5624c63308d4f389e4a9c6be61c406e0c45a356
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2022-11-02 21:54:33 -0600

    aatrox image added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { type NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
-const Home: any = async () => {
+const Home: NextPage = () => {
   const hello = trpc.example.league.useQuery();
-  const response = await fetch("https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json");
-  console.log(response);
 
-  return <div>hello</div>;
+  const leagueApiCall = async () => {
+    const response = await fetch("https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json");
+    console.log(await response.json());
+  }
+
+  return (<div onClick={leagueApiCall}>
+    <img src="https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/Aatrox.png" alt="image"></img>
+  </div>);
 };
 
 export default Home;
