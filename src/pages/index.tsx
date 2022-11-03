8724a487ccba87c8b2fe786a6d43d2b866046cd4
Roman Meredith <ROMANMEREDITH@boisestate.edu>
2022-11-02 22:31:26 -0600

    added setChampions on mount and data aatrox.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,30 @@
 import { type NextPage } from "next";
-import { trpc } from "../utils/trpc";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.league.useQuery();
+  const [champions, setChampions] = useState(null);
 
-  const leagueApiCall = async () => {
-    const response = await fetch("https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json");
-    console.log(await response.json());
+  useEffect(() => {
+    fetch(
+      "https://ddragon.leagueoflegends.com/cdn/12.4.1/data/en_US/champion.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setChampions(data));
+  });
+
+  if (!champions) {
+    return <div>...Loading</div>;
   }
 
-  return (<div onClick={leagueApiCall}>
-    <img src="https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/Aatrox.png" alt="image"></img>
-  </div>);
+  return (
+    <div>
+      {JSON.stringify(champions.data["Aatrox"])}
+      <img
+        src="https://ddragon.leagueoflegends.com/cdn/12.4.1/img/champion/Aatrox.png"
+        alt="image"
+      ></img>
+    </div>
+  );
 };
 
 export default Home;
